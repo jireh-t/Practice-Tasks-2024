0ba7459990becaaaa07167eb0a86691be1a55952
jireh-t <98923573+jireh-t@users.noreply.github.com>
2024-02-07 10:20:45 +1300

    Digit pin

--- 01_digit_pin.py
+++ 01_digit_pin.py
@@ -1,8 +1,27 @@
-pin = input("Enter your 4-digit secret pin: ")
-if pin != int:
-    print("No")
-else:
+pin = int(input("Enter your 4-digit secret pin: "))
 
-length = len(pin)
-print(length)
-print(pin)
+length = len(str(pin))
+
+while True:
+    if length > 4:
+        print("Must be 4 digits")
+        pin = int(input("Enter your 4-digit secret pin: "))
+        length = len(str(pin))
+
+    elif length == 4:
+        num = pin
+        break
+
+    elif length == 3:
+        num = f"0{pin}"
+        break
+
+    elif length == 2:
+        num = f"00{pin}"
+        break
+
+    elif length == 1:
+        num = f"000{pin}"
+        break
+
+print(f"Your pin is {num}")
