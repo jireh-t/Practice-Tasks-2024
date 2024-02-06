ce138897f7cedb565cdce3409de49a99fcbf3f41
jireh-t <98923573+jireh-t@users.noreply.github.com>
2024-02-07 10:32:18 +1300

    sandwhich

---
+++ 06_largest_number.py
@@ -0,0 +1,3 @@
+numbers = [20, 36, 12, 24, 20, 48, 74, 53, 23, 98]
+largest = max(numbers)
+print(largest)
---
+++ 07_sandwich_maker.py
@@ -0,0 +1,10 @@
+bread = {"Wholemeal": 1, "White": 0.8, "Cheesy White": 1.2, "Gluten Free": 1.4}
+meat = {"Chicken": 2.69, "Beef": 3, "Salami": 4, "Vegan Slice": 3.3}
+garnish = {"Onion": 1.69, "Tomato": 1, "Lettuce": 2, "Cheese": 2.5}
+
+bread_select = input("Please enter the number of the bread you would like:\n"
+                     "1. Wholemeal\n"
+                     "2. White\n"
+                     "3. Cheesy White\n"
+                     "4. Gluten Free\n"
+                     "")
