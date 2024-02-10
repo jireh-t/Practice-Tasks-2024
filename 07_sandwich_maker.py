ebb9c15f24f628cc9350f0191127a01dc374afcc
jireh-t <98923573+jireh-t@users.noreply.github.com>
2024-02-10 13:19:02 +1300

    Update 07_sandwich_maker.py

--- 07_sandwich_maker.py
+++ 07_sandwich_maker.py
@@ -1,11 +1,15 @@
 """Practice task problem 7 - sandwich maker"""
-
 import easygui
 
 
 # Function to ask user what bread
-def bread():
-    bread_select = easygui.buttonbox("Please select your bread:\n", "OPTIONS",
+def bread_menu():
+    bread_select = easygui.buttonbox("Please select your bread:\n"
+                                     "Wholemeal $1\n "
+                                     "White $0.80\n"
+                                     "Cheesy White $1.20\n"
+                                     "Gluten Free $1.40",
+                                     "OPTIONS",
                                      choices=["Wholemeal", "White",
                                               "Cheesy White",
                                               "Gluten Free"])
@@ -13,50 +17,119 @@ def bread():
 
 
 # Function to ask user what meat
-def meat():
-    meat_select = easygui.buttonbox("Please select your meat:\n", "OPTIONS",
-                                    choices=["Chicken", "Beef",
-                                             "Salami",
-                                             "Vegan Slice"])
-    return meat_select
+
+def meat_menu():
+    # List of meats
+    meats = ["Chicken", "Beef", "Salami", "Vegan Slice"]
+    selected_meat = []  # List of meats user has selected
+
+    # Allow user to select multiple meats
+    while True:
+        meat_select = easygui.buttonbox("Please select your meat:\n"
+                                        "Chicken $2.69\n"
+                                        " Beef $3 \n"
+                                        "Salami $4 \n"
+                                        "Vegan Slice $3.30",
+                                        "OPTIONS", meats)
+        selected_meat.append(meat_select)  # Add meat to list
+
+        # Remove option so user cannot select again
+        meats.remove(meat_select)
+        more = easygui.buttonbox("Would you like another meat?\n",
+                                 "More?",
+                                 choices=["Yes", "No"])
+        if more == "No":
+            break
+    return selected_meat
 
 
 # Function to ask user what garnish
-def garnish():
-    selected = []
+def garnish_menu():
+    # List of garnishes
+    garnishes = ["Onion", "Tomato", "Lettuce", "Cheese"]
+    selected_garnish = []  # List of garnishes user has selected
+
+    # Allow user to select multiple garnishes
     while True:
-        garnish_select = easygui.buttonbox("Please select your garnish:\n",
-                                           "OPTIONS",
-                                           choices=["Onion", "Tomato",
-                                                    "Lettuce",
-                                                    "Cheese"])
-        selected.append(garnish_select)
+        garnish_select = easygui.buttonbox("Please select your garnish:\n"
+                                           "Onion $1.69\n"
+                                           "Tomato $1\n"
+                                           "Lettuce $2\n"
+                                           "Cheese $2.50",
+                                           "OPTIONS", garnishes)
+        selected_garnish.append(garnish_select)  # Add garnish to list
+
+        # Remove option so user cannot select again
+        garnishes.remove(garnish_select)
         more = easygui.buttonbox("Would you like another garnish?\n",
-                                 "More?", choices=["Yes", "No"])
+                                 "More?",
+                                 choices=["Yes", "No"])
         if more == "No":
             break
-    return selected
+    return selected_garnish
+
+
+# Function to calculate prices
+def calculate(bread, meat, garnish):
+    bread_prices = {"Wholemeal": 1,
+                    "White": 0.8,
+                    "Cheesy White": 1.2,
+                    "Gluten Free": 1.4}
+
+    meat_prices = {"Chicken": 2.69,
+                   "Beef": 3,
+                   "Salami": 4,
+                   "Vegan Slice": 3.3}
+
+    garnish_prices = {"Onion": 1.69,
+                      "Tomato": 1,
+                      "Lettuce": 2,
+                      "Cheese": 2.5}
+
+    total = 0
+    total += bread_prices[bread]
+    for i in meat:
+        total += meat_prices[i]
+    for i in garnish:
+        total += garnish_prices[i]
+
+    return f"${total}"
+
+
+# Function to confirm or change
+def confirm(bread, meat, garnish):
+    bread_prices = {"Wholemeal": 1,
+                    "White": 0.8,
+                    "Cheesy White": 1.2,
+                    "Gluten Free": 1.4}
+
+    meat_prices = {"Chicken": 2.69,
+                   "Beef": 3,
+                   "Salami": 4,
+                   "Vegan Slice": 3.3}
+
+    garnish_prices = {"Onion": 1.69,
+                      "Tomato": 1,
+                      "Lettuce": 2,
+                      "Cheese": 2.5}
+
+    order = ""
+    for i in meat:
+        order += i
+
+    easygui.buttonbox(f"Your order is:\n"
+                      f"Bread: {bread} {bread_prices[bread]}\n"
+                      f"")
 
 
 # Main Routine
 
-breads = {"Wholemeal": 1,
-          "White": 0.8,
-          "Cheesy White": 1.2,
-          "Gluten Free": 1.4}
 
-meats = {"Chicken": 2.69,
-         "Beef": 3,
-         "Salami": 4,
-         "Vegan Slice": 3.3}
-
-garnishes = {"Onion": 1.69,
-             "Tomato": 1,
-             "Lettuce": 2,
-             "Cheese": 2.5}
-
-bread()
-meat()
-i = garnish()
-print(i)
-
+b = bread_menu()
+m = meat_menu()
+g = garnish_menu()
+cost = calculate(b, m, g)
+print(b)
+print(m)
+print(g)
+print(cost)
